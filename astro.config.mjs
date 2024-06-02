ca332c5ed056b2e1f47bdeb4624fbefe77089822
millerm30 <millergm30@gmail.com>
2024-06-02 16:23:07 -0230

    sitemap setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.michael-miller.ca',
+  integrations: [sitemap()]
 });
